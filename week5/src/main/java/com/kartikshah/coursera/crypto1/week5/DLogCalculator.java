72231fc2725273f77ba0769f33ed15002a69ffa0
Kartik Shah <shahkartikr@gmail.com>
2015-06-08 17:18:04 -0500

    Cleanup - Week 5

--- week5/src/main/java/com/kartikshah/coursera/crypto1/week5/DLogCalculator.java
+++ week5/src/main/java/com/kartikshah/coursera/crypto1/week5/DLogCalculator.java
@@ -1,8 +1,47 @@
 package com.kartikshah.coursera.crypto1.week5;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.HashMap;
+
 /**
  * Created by kartik on 2/21/15.
  */
 public class DLogCalculator
 {
+    public BigInteger calculateDLog(BigInteger p, BigInteger h, BigInteger g)
+    {
+
+        HashMap<BigInteger, BigInteger> leftHandSide = new HashMap<>();
+//        HashMap<BigInteger, BigInteger> rightHandSide = new HashMap<>();
+
+        BigInteger solutionX1 = BigInteger.ZERO;
+        BigInteger solutionX0 = BigInteger.ZERO;
+        BigInteger solutionX;
+
+
+        BigInteger B = new BigInteger("2").pow(20);
+        for (BigInteger x1 = BigInteger.ZERO; x1.compareTo(B) < 0; x1 = x1.add(BigInteger.ONE)){
+            BigInteger gx1 = g.modPow(x1, p);
+            leftHandSide.put(h.multiply(gx1.modInverse(p)).mod(p), x1);
+        }
+
+        for (BigInteger x0 = BigInteger.ZERO; x0.compareTo(B) < 0; x0 = x0.add(BigInteger.ONE)){
+            BigInteger gbx0 = g.modPow(B, p).modPow(x0,p);
+            if (leftHandSide.get(gbx0) != null){
+                solutionX1 = leftHandSide.get(gbx0);
+                solutionX0 = x0;
+                System.out.println(solutionX0 +"\n" + solutionX1);
+                break;
+            }
+        }
+
+        solutionX = solutionX0.multiply(B).add(solutionX1);
+
+        System.out.println(solutionX);
+        return solutionX;
+    }
+
+
+
 }
--- week5/src/test/java/com/kartikshah/coursera/crypto1/week5/TestDLogCalculator.java
+++ week5/src/test/java/com/kartikshah/coursera/crypto1/week5/TestDLogCalculator.java
@@ -1,8 +1,26 @@
 package com.kartikshah.coursera.crypto1.week5;
 
+import org.junit.Test;
+
+import java.math.BigInteger;
+
 /**
  * Created by kartik on 2/21/15.
  */
 public class TestDLogCalculator
 {
+    @Test
+    public void testCalculateDLog(){
+        DLogCalculator calculator = new DLogCalculator();
+        BigInteger p = new BigInteger("13407807929942597099574024998205846127479365820592393377723561443721764030073546976801874298166903427690031858186486050853753882811946569946433649006084171");
+        BigInteger h = new BigInteger("3239475104050450443565264378728065788649097520952449527834792452971981976143292558073856937958553180532878928001494706097394108577585732452307673444020333");
+        BigInteger g = new BigInteger("11717829880366207009516117596335367088558084999998952205599979459063929499736583746670572176471460312928594829675428279466566527115212748467589894601965568");
+        BigInteger x = calculator.calculateDLog(p,h,g);
+
+
+        BigInteger hx = g.modPow(x,p).mod(p);
+        System.out.println(hx);
+        System.out.println(h.mod(p));
+
+    }
 }
