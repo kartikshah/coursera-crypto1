a258680f79578178272144131ad4c7b15d81dc37
Kartik Shah <shahkartikr@gmail.com>
2015-06-08 14:06:46 -0500

    Cleanup Week1

---
+++ week5/src/test/java/com/kartikshah/coursera/crypto1/week5/TestDLogCalculator.java
@@ -0,0 +1,8 @@
+package com.kartikshah.coursera.crypto1.week5;
+
+/**
+ * Created by kartik on 2/21/15.
+ */
+public class TestDLogCalculator
+{
+}
---
+++ week6/src/test/java/com/kartikshah/coursera/crypto1/week6/TestWeek6.java
@@ -0,0 +1,8 @@
+package com.kartikshah.coursera.crypto1.week6;
+
+/**
+ * Created by kartik on 2/28/15.
+ */
+public class TestWeek6
+{
+}
